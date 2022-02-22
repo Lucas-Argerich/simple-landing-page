07a46ad183521224c2d9b1a49e5e0cd368019790
Lucas Argerich <lucasargerichcoder@gmail.com>
2022-02-22 14:14:18 -0300

    Disclaimer Section Background

--- src/pages/disclaimer/index.jsx
+++ src/pages/disclaimer/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Background from '../../components/background'
 import NavBar from '../../components/navBar'
 
 export default function Disclaimer() {
   return (
     <div>
+    <Background />
     <NavBar />
     Disclaimer</div>
   )
