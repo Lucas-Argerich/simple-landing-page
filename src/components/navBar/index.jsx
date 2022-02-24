99cc693c21b9f935083962c6d46226f519a4b408
Lucas Argerich <lucasargerichcoder@gmail.com>
2022-02-24 15:24:07 -0300

    logo link fix for githubPages

--- src/components/navBar/index.jsx
+++ src/components/navBar/index.jsx
@@ -7,7 +7,7 @@ import Button from '../button'
 export default function NavBar() {
   return (
     <nav>
-      <Link to={'/'}><Logo /></Link>
+      <Link to={'simple-landing-page/'}><Logo /></Link>
       <ul>
         <li><Link to={'simple-landing-page/'}>Home</Link></li>
         <li><Link to={'simple-landing-page/disclaimer'}>Services</Link></li>
