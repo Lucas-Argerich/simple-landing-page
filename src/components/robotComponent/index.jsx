edc5cef88ab59d6951f53c1e89af510b2d0579f3
Lucas Argerich <lucasargerichcoder@gmail.com>
2022-02-24 00:41:51 -0300

    Landing Progress & RobotComponent

---
+++ src/components/robotComponent/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ElipseFigure from './elipseFigure'
+import robotImage from './Image.png'
+
+export default function RobotComponent() {
+    return (
+        <div style={{ position: "relative" }}>
+            <img src={robotImage} />
+            <ElipseFigure left={-80} top={40} rotate={-10} opacity={1}/>
+            <ElipseFigure left={20} top={350} rotate={40} opacity={0.3} scale={0.6}/>
+            <ElipseFigure left={20} top={-50} rotate={40} opacity={0.5} scale={0.9}/>
+            <ElipseFigure left={480} top={-40} rotate={38} opacity={0.2} scale={2.5}/>
+        </div>
+    )
+}
--- src/pages/landing/index.jsx
+++ src/pages/landing/index.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import Button from '../../components/button'
+import RobotComponent from '../../components/robotComponent'
+import './index.scss'
 
 export default function Landing() {
     return (
-        <>
-        </>
+        <main>
+            <section className='headerSection'>
+                <div className='leftSide'>
+                    <h2><span>Explore</span> the fascinating world of VR</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut cursus ac ipsum sed rhoncus. Nam euismod auctor ipsum. Etiam interdum nulla neque, eu elementum arcu fermentum vel.</p>
+                    <Button text={"Click here"} />
+                </div>
+                <div className='rightSide'>
+                    <RobotComponent />
+                </div>
+            </section>
+        </main>
 
     )
 }
