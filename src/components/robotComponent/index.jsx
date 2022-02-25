75f4e221c6b931bd3555385ab19ffdf9a4a0cb7f
Lucas Argerich <lucasargerichcoder@gmail.com>
2022-02-24 22:59:33 -0300

    RobotImage Disabled Drag

--- src/components/robotComponent/index.jsx
+++ src/components/robotComponent/index.jsx
@@ -5,11 +5,11 @@ import robotImage from './Image.png'
 export default function RobotComponent() {
     return (
         <div style={{ position: "relative" }}>
-            <img src={robotImage} />
-            <ElipseFigure left={-80} top={40} rotate={-10} opacity={1}/>
-            <ElipseFigure left={20} top={350} rotate={40} opacity={0.3} scale={0.6}/>
-            <ElipseFigure left={20} top={-50} rotate={40} opacity={0.5} scale={0.9}/>
-            <ElipseFigure left={480} top={-40} rotate={38} opacity={0.2} scale={2.5}/>
+            <img src={robotImage} alt="" draggable={false} style={{ userDrag: "none", userSelect: "none" }} />
+            <ElipseFigure left={-80} top={40} rotate={-10} opacity={1} />
+            <ElipseFigure left={20} top={350} rotate={40} opacity={0.3} scale={0.6} />
+            <ElipseFigure left={20} top={-50} rotate={40} opacity={0.5} scale={0.9} />
+            <ElipseFigure left={480} top={-40} rotate={38} opacity={0.2} scale={2.5} />
         </div>
     )
 }
