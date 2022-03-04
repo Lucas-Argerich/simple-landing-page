9c373b3125e5849919b5a6cfa7409318aa79b142
Lucas Argerich <lucasargerichcoder@gmail.com>
2022-03-04 17:56:44 -0300

    Disclaimer Links fix

--- src/pages/disclaimer/index.jsx
+++ src/pages/disclaimer/index.jsx
@@ -9,8 +9,8 @@ export default function Disclaimer() {
       <AttentionIcon width={100} height={100} style={{ backdropFilter: "blur(30px)", opacity: 0.8 }} />
       <h2>This is a <span>landing page model</span>, no real services or information is shown here. </h2>
       <div className='socialMedia'>
-        <a href='https://www.linkedin.com/in/lucas-argerich/'><LinkedInIcon width={60} height={60} /></a>
-        <a href='https://github.com/Lucas-Argerich'><GithubIcon width={60} height={60} /></a>
+        <a href='https://www.linkedin.com/in/lucas-argerich/' target="_blank"><LinkedInIcon width={60} height={60} /></a>
+        <a href='https://github.com/Lucas-Argerich' target="_blank"><GithubIcon width={60} height={60} /></a>
       </div>
     </main>
   )
